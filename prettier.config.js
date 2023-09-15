8e5ab71808cd54a17fd793da9c672188049bc1a6
luangjay <zeusstyle69@gmail.com>
2023-09-16 02:41:34 +0700

    Initial commit

---
+++ prettier.config.js
@@ -0,0 +1,21 @@
+/** @type {import("@ianvs/prettier-plugin-sort-imports").PrettierConfig} */
+module.exports = {
+  endOfLine: "lf",
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  plugins: [
+    "@ianvs/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss",
+  ],
+  importOrder: [
+    "<BUILTIN_MODULES>",
+    "^(react/(.*)$)|^(react$)",
+    "^(next/(.*)$)|^(next$)",
+    "<THIRD_PARTY_MODULES>",
+    "^[./]",
+  ],
+  importOrderSortSpecifiers: true,
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+};
---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center gap-6 p-24">
+      <Image
+        src="/next.svg"
+        alt="Next.js Logo"
+        width={180}
+        height={37}
+        priority
+      />
+      <p className="text-center">
+        Get started by editing&nbsp;
+        <code className="font-mono font-bold">app/page.tsx</code>
+      </p>
+    </main>
+  );
+}
